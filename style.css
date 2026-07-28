cdb87d6e261d38dc1352ff7af1310273dc7f9456
solarenergy-umkt <2011102441111@umkt.ac.id>
2026-07-28 22:53:03 +0800

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,55 @@
+body{
 
+font-family: Arial;
+text-align:center;
+background:#f2f2f2;
+
+}
+
+
+h1{
+
+margin-top:30px;
+
+}
+
+
+
+.container{
+
+display:flex;
+justify-content:center;
+gap:20px;
+flex-wrap:wrap;
+
+}
+
+
+
+.card{
+
+background:white;
+padding:25px;
+width:150px;
+border-radius:15px;
+box-shadow:0 4px 10px #aaa;
+
+}
+
+
+
+.card span{
+
+font-size:35px;
+font-weight:bold;
+
+}
+
+
+
+canvas{
+
+max-width:800px;
+margin:auto;
+
+}
